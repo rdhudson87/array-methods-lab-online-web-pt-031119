c78f86b0f90e0ef2622bfd2a9f712c4064dd6db0
Ricardo H <ricardo.hudson923@gmail.com>
2019-03-16 21:56:36 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,21 +4,26 @@ age.include?(25)
 end
 
 def using_sort(array)
+age =[25,25,26,27]
 age.sort
 end
 
 def using_reverse(array)
+age = [25,24,26,27]
 age.reverse
 end
 
 def using_first(array)
+age = [25,24,26,27]
 age.first
 end
 
 def using_last(array)
+age = [25,24,26,27]
 age.last
 end
 
 def using_size(array)
+age = [25,24,26,27]
 age.last
 end
