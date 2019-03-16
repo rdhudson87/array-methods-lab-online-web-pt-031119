227c9a95bc253dc2482aca19ca6c1dd851a12062
Ricardo H <ricardo.hudson923@gmail.com>
2019-03-16 21:59:33 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,16 +1,13 @@
 def using_include(array, element)
 age = [24,25,26,27]
-age.include?(25)
 end
 
 def using_sort(array)
 age =[25,25,26,27]
-age.sort
 end
 
 def using_reverse(array)
 age = [25,24,26,27]
-age.reverse
 end
 
 def using_first(array)
